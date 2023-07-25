3415919328c540af912ef279c81ec91469583dac
Yunhee Won <charming9871@gmail.com>
2023-07-25 15:54:15 +0900

    [unrated] Title: 공원 산책, Time: 0.38 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.js
@@ -1,81 +1,25 @@
 function solution(park, routes) {
-  let parkMap = [];
-  let cp = [0, 0];
-
-  for (i in park) {
-    parkMap[i] = [];
-    for (let j in park[i]) {
-      parkMap[i].push(park[i][j]);
-      if (park[i][j] === "S") {
-        cp = [parseInt(i), parseInt(j)];
+    const dirs = { E: [0, 1], W: [0, -1], S: [1, 0], N: [-1, 0] };
+    // 동서남북의 경우 이런식으로 쓰는게 가독성 ㅇㅇ
+    let [x, y] = [0, 0];
+    for (let i = 0; i < park.length; i++) {
+      if (park[i].includes('S')) {
+        [x, y] = [i, park[i].indexOf('S')];
+        // park는 최대 50x50이므로 이정도는 괜찮은듯
+        break;
       }
     }
-  }
 
-  function movePosition(route) {
-    const [op, n] = route.split(" ");
-    const [x, y] = [1, 0];
-    let tmp = [...cp];
-    let i;
-
-    switch (op) {
-      case "E":
-        tmp[x] += parseInt(n);
-        if (tmp[x] < parkMap[0].length) {
-          for (i = cp[x] + 1; i <= tmp[x]; i++) {
-            if (parkMap[cp[y]][i] === "X") {
-              break;
-            }
-          }
-          cp = i - 1 === tmp[x] ? [...tmp] : cp;
-          break;
-        } else {
-          break;
-        }
-      case "W":
-        tmp[x] -= parseInt(n);
-        if (tmp[x] >= 0) {
-          for (i = cp[x] - 1; i >= tmp[x]; i--) {
-            if (parkMap[cp[y]][i] === "X") {
-              break;
-            }
-          }
-          cp = i + 1 === tmp[x] ? [...tmp] : cp;
-          break;
-        } else {
-          break;
-        }
-      case "S":
-        tmp[y] += parseInt(n);
-        if (tmp[y] < parkMap.length) {
-          for (i = cp[y] + 1; i <= tmp[y]; i++) {
-            if (parkMap[i][cp[x]] === "X") {
-              break;
-            }
-          }
-          cp = i - 1 === tmp[y] ? [...tmp] : cp;
-          break;
-        } else {
-          break;
-        }
-      case "N":
-        tmp[y] -= parseInt(n);
-        if (tmp[y] >= 0) {
-          for (i = cp[y] - 1; i >= tmp[y]; i--) {
-            if (parkMap[i][cp[x]] === "X") {
-              break;
-            }
-          }
-          cp = i + 1 === tmp[y] ? [...tmp] : cp;
-          break;
-        } else {
-          break;
-        }
-    }
+    routes.forEach((route) => {
+      const [r, n] = route.split(' ');
+      let [nx, ny] = [x, y];
+      let cnt = 0;
+      while (cnt < n) {
+        [nx, ny] = [nx + dirs[r][0], ny + dirs[r][1]];
+        if (!park[nx] || !park[nx][ny] || park[nx][ny] === 'X') break;
+        cnt++;
+      }
+      if (cnt == n) [x, y] = [nx, ny];
+    });
+    return [x, y];
   }
-
-  for (let r of routes) {
-    movePosition(r);
-  }
-  return cp;
-}
