67e0ffd1d86b82a6a186adfe4b6b50610a83bc8a
Yunhee Won <charming9871@gmail.com>
2023-08-17 20:15:32 +0900

    [unrated] Title: 크기가 작은 부분문자열, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,12 @@
+function solution(t, p) {
+    var answer = 0;
+    
+    for(let i=0; i<=(t.length-p.length); i++){
+        if(Number(t.substr(i, p.length))<=Number(p)){
+            answer ++;
+        }
+    }
+    
+    
+    return answer;
+}
