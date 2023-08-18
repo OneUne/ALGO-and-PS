31e1b927c6b73dffd8feb69eb73c3edd66495c4e
Yunhee Won <charming9871@gmail.com>
2023-08-18 15:26:14 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.24 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -1,40 +1,20 @@
-/*
-필요한 자료구조?
-- 일단 report 중복 인정 안 하니까 set 처리하고 --> new Set();
-- 각 user가 신고한 사람들을 어떻게 관리할 것인가. --> map 두 개 만들어서 하나는 신고자, 하나는 신고당한 횟수 이렇게?
-
-*/
-
 function solution(id_list, report, k) {
-    var answer = [];
-    const reportSet = new Set(report);
-    var reportMap = {};
-    var reportNum = {};
-    
-    for(let i of reportSet){
-        const [reporter, reportee] = i.split(' ')
-        
-        if(!reportMap[reporter]) reportMap[reporter] = reportee;
-        else reportMap[reporter] += ' '+reportee
-        
-        if(!reportNum[reportee]) reportNum[reportee] = 1;
-        else reportNum[reportee]++;
-        
+    const reportedUsers = new Set(report);
+    const reportMap = new Map();
+    const reportCounts = {};
+
+    for (const report of reportedUsers) {
+        const [reporter, reportee] = report.split(' ');
+
+        reportMap.set(reporter, (reportMap.get(reporter) || []).concat(reportee));
+
+        reportCounts[reportee] = (reportCounts[reportee] || 0) + 1;
     }
-    
-    for (let i of id_list){
-        if(!reportMap[i]){
-            answer.push(0);
-        } else{
-            var blockNum = 0;
-            reportMap[i].split(' ').map((v)=>{
-                if(reportNum[v]>=k){
-                    blockNum++;
-                }
-            })
-            answer.push(blockNum);
-        }
-    }
-    
+
+    const answer = id_list.map((user) => {
+        const blockedCount = (reportMap.get(user) || []).filter((reportedUser) => reportCounts[reportedUser] >= k).length;
+        return blockedCount;
+    });
+
     return answer;
 }
