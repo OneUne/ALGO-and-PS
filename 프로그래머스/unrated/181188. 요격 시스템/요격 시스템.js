d6f503198cb82be39b59ea6a9428bbf6d4d3b8a9
Yunhee Won <charming9871@gmail.com>
2023-08-21 14:44:08 +0900

    [unrated] Title: 요격 시스템, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.js
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.js
@@ -4,22 +4,17 @@
 */
 
 function solution(targets) {
-    var answer = 1;
-    
-    targets.sort(function(a,b){
-        return a[0]-b[0]
-    })
-    
-    var cp = targets[0]
-    
-    targets.forEach((v)=>{
-        if(v[0]<cp[1]){
-            cp = [v[0], Math.min(cp[1],v[1])]
-        } else{
-            cp = v;
-            answer++;
+    targets.sort((a, b) => a[0] - b[0]);
+
+    let answer = targets.reduce((count, [start, end]) => {
+        if (start < count.end) {
+            count.end = Math.min(count.end, end);
+        } else {
+            count.end = end;
+            count.total++;
         }
-    })
-    
-    return answer;
+        return count;
+    }, { end: -Infinity, total: 0 });
+
+    return answer.total;
 }
