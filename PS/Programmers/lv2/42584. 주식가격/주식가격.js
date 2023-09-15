fdf9671020e86eb09f718c1140eb1240e6c66491
Yunhee Won <yunhee.won@radiusxr.com>
2023-09-15 17:10:38 +0900

    주식가격.js 파일 improvedVer이랑 같아서 수정

--- PS/Programmers/lv2/42584. 주식가격/주식가격.js
+++ PS/Programmers/lv2/42584. 주식가격/주식가격.js
@@ -1,19 +1,14 @@
 function solution(prices) {
-    const answer = new Array(prices.length).fill(0);
-    const stack = [];
-
-    for (let i = 0; i < prices.length; i++) {
-        while (stack.length > 0 && prices[i] < prices[stack[stack.length - 1]]) {
-            const top = stack.pop();
-            answer[top] = i - top;
-        }
-        stack.push(i);
+  var answer = [];
+  for (let i = 0; i < prices.length; i++) {
+    for (let j = i + 1; j < prices.length; j++) {
+      if (prices[i] > prices[j]) {
+        answer.push(j - i);
+        break;
+      }
+      if (j === prices.length - 1) answer.push(j - i);
     }
-
-    while (stack.length > 0) {
-        const top = stack.pop();
-        answer[top] = prices.length - 1 - top;
-    }
-
-    return answer;
+  }
+  answer.push(0);
+  return answer;
 }
