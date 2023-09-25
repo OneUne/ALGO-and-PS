bc5ca488b8cbf55c8f205bf7b9cfee443b17e432
Yunhee Won <charming9871@gmail.com>
2023-09-25 14:53:36 +0900

    [level 2] Title: 가장 큰 수, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,4 @@
+function solution(numbers) {
+    let sortedString = numbers.map((v)=>v.toString()).sort((a,b)=>(b+a)-(a+b)).join("")
+    return parseInt(sortedString) === 0 ? "0" : sortedString;
+}
